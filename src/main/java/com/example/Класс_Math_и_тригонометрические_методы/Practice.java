f01cb0c4ec3d12a5508111243d3a4fd018f371e4
Bayakan <spring.boot@hotmail.com>
2024-04-22 16:53:59 +0300

    Класс Math и тригонометрические методы

---
+++ src/main/java/com/example/Класс_Math_и_тригонометрические_методы/Practice.java
@@ -0,0 +1,14 @@
+package com.example.Класс_Math_и_тригонометрические_методы;
+
+public class Practice {
+    public static void main(String[] args) {
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+        System.out.println(Math.sin(Math.PI/6));
+        System.out.println(Math.cos(Math.PI));
+        System.out.println(Math.tan(Math.PI/4));
+        System.out.println(Math.asin(0.5));
+        System.out.println(Math.toRadians(30));
+        System.out.println(Math.toDegrees(Math.PI/6));
+    }
+}
