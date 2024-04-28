b2a18a2b88bedee21703931b3cecf59aed3b0cd7
Bayakan <spring.boot@hotmail.com>
2024-04-28 14:58:23 +0300

    Character класс: isDigit, isLetter, isUpperCase, isLowerCase, toUpperCase

---
+++ src/main/java/com/example/Character_класс/Practice.java
@@ -0,0 +1,13 @@
+package com.example.Character_класс;
+
+public class Practice {
+    public static void main(String[] args) {
+        char c = 'a';
+        System.out.println(Character.isDigit(c));
+        System.out.println(Character.isLetter(c));
+        System.out.println(Character.isLetterOrDigit(c));
+        System.out.println(Character.isLowerCase(c));
+        System.out.println(Character.isUpperCase(c));
+        System.out.println(Character.isUpperCase(c));
+    }
+}
