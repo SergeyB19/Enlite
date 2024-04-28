babd3ef7ad04ffe58a696d92bd98ea2ee40d2061
Bayakan <spring.boot@hotmail.com>
2024-04-28 14:51:35 +0300

    Класс Math и min, max, abs, random методы

---
+++ src/main/java/com/example/Класс_Math_и_тригонометрические_методы/Practice3.java
@@ -0,0 +1,11 @@
+package com.example.Класс_Math_и_тригонометрические_методы;
+
+public class Practice3 {
+    public static void main(String[] args) {
+        System.out.println("Math.min(3,5) = " + Math.min(3, 5));
+        System.out.println("Math.max(3,5) = " + Math.max(3, 5));
+        System.out.println("Math.abs(-7) = " + Math.abs(-7));
+        System.out.println("Math.abs(7) = " + Math.abs(7));
+        System.out.println("Math.random() = " + Math.random());
+    }
+}
