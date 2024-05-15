8e51a31ebcfb0990eff6b0680c082e09732edb94
Bayakan <spring.boot@hotmail.com>
2024-05-15 11:26:08 +0300

    ООП. Метод toString()

---
+++ src/main/java/com/example/OOP/ToString/ToString.java
@@ -0,0 +1,11 @@
+package com.example.OOP.ToString;
+
+public class ToString {
+    public static void main(String[] args) {
+        String s = new String("Jack");
+        System.out.println(s);
+        System.out.println(s.toString());
+        Example example = new Example("John", 1);
+        System.out.println(example);
+    }
+}
