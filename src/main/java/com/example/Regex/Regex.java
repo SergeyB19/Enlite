b0f23ddc0f97c78c698b4f45ae0471da441d2953
Bayakan <spring.boot@hotmail.com>
2024-05-16 18:03:00 +0300

    Regex(регулярные выражения). a-z A-Z 0-9

--- src/main/java/com/example/Regex/Regex.java
+++ src/main/java/com/example/Regex/Regex.java
@@ -2,18 +2,13 @@ package com.example.Regex;
 
 public class Regex {
     public static void main(String[] args) {
-        String s = "Jqwa";
+        String s = "JA!a";
 
-        if (s.matches("J..a")) {
+        if (s.matches("J[a-zA-Z0-9].a")) {
             System.out.println("YES");
 
         } else {
             System.out.println("NO");
-        }
-        if (s.equals("Java")) {
-            System.out.println("Equals");
-        } else {
-            System.out.println("Not Equals");
         }
     }
 }
