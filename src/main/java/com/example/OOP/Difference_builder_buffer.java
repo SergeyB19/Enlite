f118f75803f223989207b5b2f2fbc8b6e430a0bb
Bayakan <spring.boot@hotmail.com>
2024-05-16 17:04:40 +0300

    ООП. Отличие StringBuilder от StringBuffer

---
+++ src/main/java/com/example/OOP/Difference_builder_buffer.java
@@ -0,0 +1,18 @@
+package com.example.OOP;
+
+public class Difference_builder_buffer {
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder();
+        StringBuffer buffer = new StringBuffer();
+
+        builder.append("abc");
+        builder.deleteCharAt(1);
+        builder.setCharAt(0,'w');
+        System.out.println(builder.toString());
+
+        buffer.append("abc");
+        buffer.deleteCharAt(1);
+        buffer.setCharAt(0,'q');
+        System.out.println(buffer.toString());
+    }
+}
