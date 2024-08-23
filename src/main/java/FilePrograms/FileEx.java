23699b4e1ff01a5f5713be986210de4ea23b3a2f
Bayakan <spring.boot@hotmail.com>
2024-08-23 17:10:03 +0300

    Ввод-вывод. Классы File и Scanner

--- src/main/java/FilePrograms/FileEx.java
+++ src/main/java/FilePrograms/FileEx.java
@@ -3,14 +3,22 @@ package FilePrograms;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class FileEx {
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("out.txt");
-        PrintWriter printWriter = new PrintWriter(file);
+        Scanner in = new Scanner(file);
+        while (in.hasNext()) {
+            String s = in.next();
+            System.out.print(s + " ");
+        }
+        in.close();
+
+      /*  PrintWriter printWriter = new PrintWriter(file);
         printWriter.println("Hello");
         printWriter.println("World");
-        printWriter.close();
+        printWriter.close();*/
 
     }
 }
