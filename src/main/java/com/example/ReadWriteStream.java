7134cb08f72e86ecf29db9c20054b057a29a5220
Bayakan <spring.boot@hotmail.com>
2024-09-05 22:02:38 +0300

    Ввод-вывод. Классы InputStream, OutputStream, DataInputStream, DataOutputStream

--- src/main/java/com/example/ReadWriteStream.java
+++ src/main/java/com/example/ReadWriteStream.java
@@ -5,7 +5,17 @@ import java.io.*;
 public class ReadWriteStream {
     public static void main(String[] args) throws IOException {
         File file = new File("output.txt");
-        PrintWriter out = new PrintWriter(file);
+        DataOutputStream out = new DataOutputStream(new FileOutputStream(file));
+        DataInputStream in = new DataInputStream(new FileInputStream(file));
+        out.writeUTF("Privet");
+        out.write(123);
+        out.close();
+
+        System.out.println(in.readUTF());
+        System.out.println(in.read());
+        in.close();
+
+       /* PrintWriter out = new PrintWriter(file);
         FileReader in = new FileReader(file);
 
         out.print("Hello");
@@ -16,6 +26,6 @@ public class ReadWriteStream {
         while ((temp = in.read()) != -1) {
             System.out.print((char) temp);
         }
-        in.close();
+        in.close();*/
     }
 }
