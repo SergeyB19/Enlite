0e253f3d881b8f71ff7206c825e9dbc5d0db62e2
Bayakan <spring.boot@hotmail.com>
2024-09-09 21:35:07 +0300

    Сеть. Сервер-Клиент соединение

--- src/main/java/com/example/socket/ClientSocket.java
+++ src/main/java/com/example/socket/ClientSocket.java
@@ -3,15 +3,28 @@ package com.example.socket;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ClientSocket {
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket("127.0.0.1", 8081);
         PrintWriter out = new PrintWriter(socket.getOutputStream());
-        out.println("Hello");
-        out.println("World");
-        out.println("How are you?");
-        out.close();
-        socket.close();
+        Scanner in = new Scanner(socket.getInputStream());
+        Scanner send = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Please enter something to send to server:");
+            String words = send.nextLine();
+            out.println(words);
+            out.flush();
+            out.println("Hello");
+            out.println("World");
+            out.println("How are you?");
+            String s = in.nextLine();
+            System.out.println("Data from server has come " + s);
+        }
+
+//        out.close();
+//        socket.close();
     }
 }
--- src/main/java/com/example/socket/Server.java
+++ src/main/java/com/example/socket/Server.java
@@ -1,6 +1,7 @@
 package com.example.socket;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
@@ -10,9 +11,17 @@ public class Server {
         ServerSocket serverSocket = new ServerSocket(8081);
         Socket input = serverSocket.accept();
         Scanner in = new Scanner(input.getInputStream());
+        PrintWriter out = new PrintWriter(input.getOutputStream());
+        Scanner send = new Scanner(System.in);
         while (in.hasNext()) {
+            System.out.println("Data is ome from client: ");
             System.out.println(in.nextLine());
+            System.out.println("Please write what you want to sent to client");
+            String s = send.nextLine();
+            out.println(s);
+            out.flush();
         }
+        out.close();
         in.close();
         input.close();
         serverSocket.close();
