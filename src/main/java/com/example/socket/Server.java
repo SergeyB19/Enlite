2c4ff279a386edea62daabcf7fa456cd880cfaa0
Bayakan <spring.boot@hotmail.com>
2024-09-12 22:05:24 +0300

    Сеть. Сервер-Клиент соединение. Продолжение

--- src/main/java/com/example/socket/Server.java
+++ src/main/java/com/example/socket/Server.java
@@ -9,21 +9,37 @@ import java.util.Scanner;
 public class Server {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(8081);
-        Socket input = serverSocket.accept();
-        Scanner in = new Scanner(input.getInputStream());
-        PrintWriter out = new PrintWriter(input.getOutputStream());
-        Scanner send = new Scanner(System.in);
-        while (in.hasNext()) {
-            System.out.println("Data is ome from client: ");
-            System.out.println(in.nextLine());
-            System.out.println("Please write what you want to sent to client");
-            String s = send.nextLine();
-            out.println(s);
-            out.flush();
+        while (true) {
+            Socket sock = serverSocket.accept();
+            Thread t = new Thread(new ClientHandler(sock));
+            t.start();
         }
-        out.close();
-        in.close();
-        input.close();
-        serverSocket.close();
+    }
+}
+
+class ClientHandler implements Runnable {
+    Socket socket;
+
+    public ClientHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Scanner in = new Scanner(socket.getInputStream());
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
+            Scanner send = new Scanner(System.in);
+            while (in.hasNext()) {
+                System.out.println("Data is ome from client: ");
+                System.out.println(in.nextLine());
+                System.out.println("Please write what you want to sent to client");
+                String s = send.nextLine();
+                out.println(s);
+                out.flush();
+            }
+        } catch (Exception e) {
+
+        }
     }
 }
