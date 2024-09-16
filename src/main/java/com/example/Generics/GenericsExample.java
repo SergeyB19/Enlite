6d408bda6c82a2aabadd0b2f5b453fcca1015c47
Bayakan <spring.boot@hotmail.com>
2024-09-16 20:28:34 +0300

    Дженерики. Добавление нескольких типов.

--- src/main/java/com/example/Generics/GenericsExample.java
+++ src/main/java/com/example/Generics/GenericsExample.java
@@ -3,17 +3,17 @@ package com.example.Generics;
 public class GenericsExample {
     public static void main(String[] args) {
         Integer a = 1;
-        Integer b = 2;
-        show(a,b);
+        Double b = 2.2;
+        show(a, b);
         Long c = 3L;
         Long d = 4L;
-        show(c,d);
+        show(c, d);
         String s = "ABC";
         String t = "FCG";
-        show(s,t);
+        show(s, t);
     }
 
-    public static <T> void show(T a, T b) {
+    public static <T, U> void show(T a, U b) {
         System.out.println(a + " " + b);
     }
 
