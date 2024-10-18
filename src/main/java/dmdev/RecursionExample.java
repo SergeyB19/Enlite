9589242ea1e14a9d30e121d1f65ee2224277bdca
Bayakan <spring.boot@hotmail.com>
2024-10-18 18:51:15 +0300

    Рекурсия

---
+++ src/main/java/dmdev/RecursionExample.java
@@ -0,0 +1,17 @@
+package dmdev;
+
+public class RecursionExample {
+    public static void main(String[] args) {
+        int factorial = factorial(6);
+        System.out.println(factorial);
+
+    }
+
+    public static int factorial(int value) {
+        if (value == 1) {
+            return 1;
+        }
+
+        return value * factorial(value - 1);
+    }
+}
