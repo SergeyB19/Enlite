a8be95f524387f8918b1ac75e1868ae3e2cb443e
Bayakan <spring.boot@hotmail.com>
2024-10-27 13:26:46 +0300

    Многопоточность. Synchronized

--- src/main/java/com/example/Threads/Main.java
+++ src/main/java/com/example/Threads/Main.java
@@ -2,7 +2,20 @@ package com.example.Threads;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("Hello World" + " " + Thread.currentThread().getName());
+        MyThread myThread = new MyThread();
+
+        Thread t1 = new Thread(myThread);
+        t1.start();
+//        Thread.sleep(1000);
+
+        Thread t2 = new Thread(myThread);
+        t2.start();
+        Thread.sleep(1000);
+
+        System.out.println(myThread.getX());
+
+
+        /*System.out.println("Hello World" + " " + Thread.currentThread().getName());
         MyThread myThread = new MyThread();
         myThread.start();
 
@@ -18,27 +31,36 @@ public class Main {
         Thread t2 = new Thread(new MyThreadRunnable());
         t2.start();
 
-        System.out.println("Main thread is dead");
+        System.out.println("Main thread is dead");*/
     }
 
-    public static void print() {
+    /*public static void print() {
         System.out.println("Ok I print");
-    }
+    }*/
 }
 
 
 class MyThread extends Thread {
+    int x = 0;
     @Override
-    public void run() {
-        for (int i = 0; i < 100; i++) {
+    public synchronized void run() {
+        for (int i = 0; i < 10000; i++) {
+            int temp = x;
+            temp++;
+            x = temp;
 
-            System.out.println(i + " Hello from MyThread"+ " " + Thread.currentThread().getName());
-            Thread.yield();
+          /*  System.out.println(i + " Hello from MyThread"+ " " + Thread.currentThread().getName());
+            Thread.yield();*/
         }
+    }
+
+    public int getX() {
+        return x;
     }
 }
 
 
+/*
 class MyThreadRunnable implements Runnable {
 
     @Override
@@ -50,3 +72,4 @@ class MyThreadRunnable implements Runnable {
 
     }
 }
+*/
