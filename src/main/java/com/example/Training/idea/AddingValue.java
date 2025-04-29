c29cdb7015a20b55f6ebe8ee2a349980942a1e3e
Bayakan <spring.boot@hotmail.com>
2025-04-29 20:04:37 +0300

    AddingValue

---
+++ src/main/java/com/example/Training/idea/AddingValue.java
@@ -0,0 +1,30 @@
+package com.example.Training.idea;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AddingValue {
+
+
+    // write a method here
+
+    public static void addValueByIndex(long[] arr, int i, long l) {
+        if (i >= 0 && i < arr.length) {
+            arr[i] += l;
+        }
+    }
+
+
+    // don't change the code below
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long[] array = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToLong(Long::parseLong)
+                .toArray();
+        int index = scanner.nextInt();
+        long value = scanner.nextLong();
+        addValueByIndex(array, index, value);
+        Arrays.stream(array).forEach(e -> System.out.print(e + " "));
+    }
+}
+
