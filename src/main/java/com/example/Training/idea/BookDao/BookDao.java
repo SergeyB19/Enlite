7ec8b677cf2c39bbfff3dad4ee034f5e394b9669
Bayakan <spring.boot@hotmail.com>
2025-06-04 21:15:52 +0300

    Map-based implementation

---
+++ src/main/java/com/example/Training/idea/BookDao/BookDao.java
@@ -0,0 +1,139 @@
+package com.example.Training.idea.BookDao;
+
+import java.util.*;
+
+interface BookDao {
+
+    void add(Book book);
+
+    Book get(int id);
+
+    void update(Book book);
+
+    void delete(int id);
+}
+
+class BookDaoImpl implements BookDao {
+    private final Map<Integer, Book> books;
+
+    public BookDaoImpl() {
+        this.books = new HashMap<>();
+    }
+
+    @Override
+    public void add(Book book) {
+        if (book == null) {
+            throw new NullPointerException("null");
+        } else if (books.containsKey(book.getId())) {
+            throw new RuntimeException("the book already exists in the map");
+        } else {
+            books.put(book.getId(), book);
+        }
+    }
+
+    @Override
+    public Book get(int id) {
+        Book book = books.get(id);
+
+        return book;
+    }
+
+    @Override
+    public void update(Book book) {
+        if (book == null) {
+            throw new NullPointerException("null");
+        } else if (books.containsKey(book.getId())) {
+            books.put(book.getId(), book);
+        } else {
+            throw new RuntimeException("Book is not found");
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+        if (books.get(id) == null) {
+            throw new RuntimeException("Book not found");
+        } else {
+            books.remove(id);
+        }
+    }
+}
+
+class Book {
+    private int id;
+    private String title;
+
+    public Book(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Book() {
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [id " + id + ", title : " + title + "]";
+    }
+}
+
+/* Do not change code below */
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BookDao bookDao = new BookDaoImpl();
+
+        int id1 = scanner.nextInt();
+        scanner.nextLine();
+        String title1 = scanner.nextLine();
+        Book book1 = new Book(id1, title1);
+
+        int id2 = scanner.nextInt();
+        scanner.nextLine();
+        String title2 = scanner.nextLine();
+        Book book2 = new Book(id2, title2);
+
+        int inexistentId = scanner.nextInt();
+
+        bookDao.add(book1);
+        bookDao.add(book2);
+
+        // get first
+        System.out.println("Found " + bookDao.get(book1.getId()));
+
+        // get inexistent book
+        if (bookDao.get(inexistentId) == null) {
+            System.out.println("Not found id " + inexistentId);
+        }
+
+        // update and get
+        Book updatedBook = bookDao.get(book2.getId());
+        System.out.println("Found " + updatedBook);
+        updatedBook.setTitle("UPDATED");
+        bookDao.update(updatedBook);
+        System.out.println("Updated " + bookDao.get(book2.getId()));
+
+        // delete
+        bookDao.delete(book2.getId());
+        if (bookDao.get(book2.getId()) == null) {
+            System.out.println("Deleted id: " + book2.getId());
+        }
+    }
+}
